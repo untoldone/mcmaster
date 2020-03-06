eca12fdc2585c3ac1aaa854f93f0fa3ec90be9f5
Michael M. Wasser <untoldone@users.noreply.github.com>
2020-03-06 15:57:37 -0800

    signin template

---
+++ src/routes/signin.svelte
@@ -0,0 +1,43 @@
+<div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+  <div class="max-w-md w-full">
+    <div>
+      <svg class="mx-auto h-16 w-auto" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 80 80" x="0px" y="0px">
+        <path fill="#5850EC" d="M69.85,19.3,43,3.8a6,6,0,0,0-6,0L10.15,19.3a6,6,0,0,0-3,5.2V34a2,2,0,0,0,4,0V25.66l11.32,6.53a5.09,5.09,0,0,0,0,.55V45.52L10.15,38.44a2,2,0,0,0-3,1.73V55.5a6,6,0,0,0,3,5.2L37,76.2a6,6,0,0,0,6,0L69.85,60.7a6,6,0,0,0,3-5.2v-31A6,6,0,0,0,69.85,19.3ZM22.43,63.17,12.15,57.23a2,2,0,0,1-1-1.73V43.63l11.28,6.51ZM39,7.27a2,2,0,0,1,2,0L51.28,13.2,40,19.71,28.72,13.2ZM24.47,28.72,13.15,22.19l11.57-6.68L36,22,24.93,28.41ZM38,25.49V38.87L26.43,45.53V32.74a1,1,0,0,1,.5-.86Zm0,46.66L26.43,65.48v-13L37.5,58.84a3.33,3.33,0,0,0,.5.25Zm1.5-16.77L28.44,49,40,42.33,51.56,49,40.5,55.38A1,1,0,0,1,39.5,55.38Zm14.07,10.1L42,72.15V59.09a3.33,3.33,0,0,0,.5-.25l11.07-6.39Zm0-20L42,38.87V25.49l11.07,6.39a1,1,0,0,1,.5.86Zm1.5-17.12L44,22l11.28-6.51,11.57,6.68L55.53,28.72ZM68.85,55.5a2,2,0,0,1-1,1.73L57.57,63.17v-13l8.25-4.76a2,2,0,1,0-2-3.47l-6.25,3.61V32.74a5.09,5.09,0,0,0,0-.55l11.32-6.53Z"/>
+      </svg>
+      <h2 class="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
+        Sign in to your account
+      </h2>
+    </div>
+    <form class="mt-8" action="#" method="POST">
+      <input type="hidden" name="remember" value="true" />
+      <div class="rounded-md shadow-sm">
+        <div>
+          <input aria-label="Minecraft Username" name="username" type="text" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5" placeholder="Minecraft Username" />
+        </div>
+        <div class="-mt-px">
+          <input aria-label="Password" name="password" type="password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5" placeholder="Password" />
+        </div>
+      </div>
+
+      <div class="mt-6 flex items-center justify-between">
+        <div class="flex items-center">
+          <input id="remember_me" type="checkbox" class="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" />
+          <label for="remember_me" class="ml-2 block text-sm leading-5 text-gray-900">
+            Remember me
+          </label>
+        </div>
+      </div>
+
+      <div class="mt-6">
+        <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+          <span class="absolute left-0 inset-y pl-3">
+            <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150" fill="currentColor" viewBox="0 0 20 20">
+              <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
+            </svg>
+          </span>
+          Sign in
+        </button>
+      </div>
+    </form>
+  </div>
+</div>
